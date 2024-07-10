04c545d17c604544496b4093d129355fbd46535a
david <david-khala@hotmail.com>
2024-07-10 20:53:36 +0800

    syntax support

--- testpathdeps/src/main.rs
+++ testpathdeps/src/main.rs
@@ -4,11 +4,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use bigdecimal::BigDecimal;
-    use bigdecimal::num_bigint::{BigInt, ToBigInt};
 
-    fn bigint_100() -> BigInt {
-        return 100.to_bigint().unwrap();
-    }
     #[test]
     fn test_decimal_from_int() {
         let dec = BigDecimal::from(1);
