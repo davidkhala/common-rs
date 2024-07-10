4b2e2e01799a95b08422c2f18cda4c05a036916b
David Liu <david-khala@hotmail.com>
2024-07-10 15:41:29 +0800

    add syntax test

---
+++ testpathdeps/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+
+}
+#[cfg(test)]
+mod tests {
+    use bigdecimal::BigDecimal;
+    use bigdecimal::num_bigint::{BigInt, ToBigInt};
+
+    fn bigint_100() -> BigInt {
+        return 100.to_bigint().unwrap();
+    }
+    #[test]
+    fn test_decimal_from_int() {
+        let dec = BigDecimal::from(1);
+        println!("{}", dec);
+    }
+}
