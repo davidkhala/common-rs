f891dc6cec34d64109a4659a2f6d5bacec4b4ca7
david <david-khala@hotmail.com>
2024-07-16 18:50:34 +0800

    syntax support

--- testpathdeps/src/main.rs
+++ testpathdeps/src/main.rs
@@ -1,6 +1,4 @@
-fn main() {
-
-}
+fn main() {}
 #[cfg(test)]
 mod tests {
     use bigdecimal::BigDecimal;
@@ -9,5 +7,25 @@ mod tests {
     fn test_decimal_from_int() {
         let dec = BigDecimal::from(1);
         println!("{}", dec);
+    }
+    #[test]
+    fn get_sys_env() {
+        use std::env;
+
+        let mut handlers = vec![];
+        let text ="".to_string();
+        handlers.push(std::thread::spawn(move || {
+            println!("{:?}", env::var_os("HOME").unwrap());
+            println!("{}", text)
+        }));
+
+        handlers.push(std::thread::spawn(move || {
+            println!("{:?}", env::var_os("HOME").unwrap());
+            // TODO assert throw here
+            println!("{}", text)
+        }));
+        for h in handlers {
+            h.join().unwrap();
+        }
     }
 }
