ff85954cb3ab2766167e801b44b58294768d61fc
David Liu <david-khala@hotmail.com>
2024-09-22 12:12:21 +0800

    test env whether OPENSSL_DIR: C:\Program Files\OpenSSL is required

--- ssl/win/desktop.ps1
+++ ssl/win/desktop.ps1
@@ -1,4 +1,10 @@
-$env:VCPKG_ROOT="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\vcpkg"
-#choco install -y openssl --force
-$env:OPENSSL_DIR="C:\Program Files\OpenSSL-Win64"
-$env:OPENSSL_LIB_DIR="C:\Program Files\OpenSSL-Win64\lib\VC\x64\MD"
+# $env:VCPKG_ROOT="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\vcpkg"
+[Environment]::SetEnvironmentVariable("VCPKG_ROOT", "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\vcpkg", "User")
+
+choco install -y openssl --force
+
+# $env:OPENSSL_DIR="C:\Program Files\OpenSSL-Win64"
+[Environment]::SetEnvironmentVariable("OPENSSL_DIR", "C:\Program Files\OpenSSL-Win64", "User")
+
+# $env:OPENSSL_LIB_DIR="C:\Program Files\OpenSSL-Win64\lib\VC\x64\MD"
+[Environment]::SetEnvironmentVariable("OPENSSL_LIB_DIR", "C:\Program Files\OpenSSL-Win64\lib\VC\x64\MD", "User")
